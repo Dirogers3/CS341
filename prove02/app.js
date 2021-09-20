69196f4efe829ac62d9ce0a9fd3a100f213f2ab0
dirogers <rog17006@byui.edu>
2021-09-20 11:40:23 -0700

    Update to fix heroku configs

--- prove02/app.js
+++ prove02/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const http = require('http');
 
 const routes = require('./routes/routes.js');
 
@@ -18,7 +19,6 @@ app.use((req, res, next) => {
     res.status(404).send('<h1>404 Page not found.</h1>')
 })
 
-
-
-
-app.listen(3000);
+const server = http.createServer()
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
