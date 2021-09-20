793f2d4cbf3c2860bacf65321320c70d31dd2846
dirogers <rog17006@byui.edu>
2021-09-20 12:03:59 -0700

    Removed Pug and added EJS

--- prove02/app.js
+++ prove02/app.js
@@ -7,7 +7,7 @@ const routes = require('./routes/routes.js');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
